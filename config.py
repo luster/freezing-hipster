ddf07514d7203532e0dccd700647b9ec93e520d6
Ethan Lusterman <ethan.lusterman@gmail.com>
2014-04-27 15:14:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = '2435908a7wfuioaj349a7weofawuifhaeowf78a34'
+SECRET_KEY = 'your-secret-key'
 
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' }
